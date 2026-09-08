82db112c3bb00b24961a61ce68d05db6cb1129ed
sawelu <sawelu@users.noreply.github.com>
2026-09-08 22:39:00 +0700

    Звуки из архива game_sounds (синтезированные заглушки) вместо прежних

--- src/games/types/CatGame.tsx
+++ src/games/types/CatGame.tsx
@@ -59,7 +59,7 @@ export default function CatGame({ gameId, onScoreUpdate }: { gameId: string; onS
                 <div
                   onClick={() => {
                     pet()
-                    api.add(2, 'meow')
+                    api.add(2, 'purr')
                   }}
                   className="cat-face"
                   style={{
